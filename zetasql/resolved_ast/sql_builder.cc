b4f10416362475a6cbeaaa51b209f57a236d7642
Dmitry Boykov <dmitry.boykov@fivetran.com>
2022-11-10 15:06:32 +0100

    Revert generation of alias names

--- zetasql/resolved_ast/sql_builder.cc
+++ zetasql/resolved_ast/sql_builder.cc
@@ -110,8 +110,7 @@ std::string SQLBuilder::GetColumnAlias(const ResolvedColumn& column) {
     return zetasql_base::FindOrDie(computed_column_alias_, column.column_id());
   }
 
-  const std::string alias = column.name().empty() || IsInternalAlias(column.name())
-      ? GenerateUniqueAliasName() : column.name();
+  const std::string alias = GenerateUniqueAliasName();
   zetasql_base::InsertOrDie(&computed_column_alias_, column.column_id(), alias);
   return alias;
 }
