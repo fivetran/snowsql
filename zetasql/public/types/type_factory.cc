a0ddc53971df2a6f907bde03e4f1b76f3ba6282f
Pavel Grivachev <pavel@fivetran.com>
2023-06-12 19:52:19 +0200

    Add object type

--- zetasql/public/types/type_factory.cc
+++ zetasql/public/types/type_factory.cc
@@ -303,6 +303,7 @@ const Type* TypeFactory::get_numeric() { return types::NumericType(); }
 const Type* TypeFactory::get_bignumeric() { return types::BigNumericType(); }
 const Type* TypeFactory::get_json() { return types::JsonType(); }
 const Type* TypeFactory::get_variant() { return types::VariantType(); }
+const Type* TypeFactory::get_object() { return types::ObjectType(); }
 
 const Type* TypeFactory::MakeSimpleType(TypeKind kind) {
   ABSL_CHECK(Type::IsSimpleType(kind)) << kind;
@@ -333,6 +334,7 @@ absl::Status TypeFactory::MakeArrayType(const Type* element_type,
       types::BigNumericType(),
       types::JsonType(),
       types::VariantType(),
+      types::ObjectType(),
   };
   if (this != s_type_factory() && kStaticTypeSet->contains(element_type)) {
     return s_type_factory()->MakeArrayType(element_type, result);
@@ -974,6 +976,11 @@ static const Type* s_variant_type() {
   return s_variant_type;
 }
 
+static const Type* s_object_type() {
+  static const Type* s_object_type = new SimpleType(s_type_factory(), TYPE_OBJECT);
+  return s_object_type;
+}
+
 static const EnumType* s_date_part_enum_type() {
   static const EnumType* s_date_part_enum_type = [] {
     const EnumType* enum_type;
@@ -1169,6 +1176,12 @@ static const ArrayType* s_variant_array_type() {
   return s_variant_array_type;
 }
 
+static const ArrayType* s_object_array_type() {
+  static const ArrayType* s_object_array_type =
+      MakeArrayType(s_type_factory()->get_object());
+  return s_object_array_type;
+}
+
 static const EnumType* GetArrayZipModeEnumType() {
   static const EnumType* s_array_zip_mode_enum_type = [] {
     const EnumType* enum_type;
@@ -1203,6 +1216,7 @@ const Type* NumericType() { return s_numeric_type(); }
 const Type* BigNumericType() { return s_bignumeric_type(); }
 const Type* JsonType() { return s_json_type(); }
 const Type* VariantType() { return s_variant_type(); }
+const Type* ObjectType() { return s_object_type(); }
 const StructType* EmptyStructType() { return s_empty_struct_type(); }
 const EnumType* DatePartEnumType() { return s_date_part_enum_type(); }
 const EnumType* NormalizeModeEnumType() { return s_normalize_mode_enum_type(); }
@@ -1244,6 +1258,8 @@ const ArrayType* JsonArrayType() { return s_json_array_type(); }
 
 const ArrayType* VariantArrayType() { return s_variant_array_type(); }
 
+const ArrayType* ObjectArrayType() { return s_object_array_type(); }
+
 const Type* TypeFromSimpleTypeKind(TypeKind type_kind) {
   switch (type_kind) {
     case TYPE_INT32:
@@ -1284,6 +1300,8 @@ const Type* TypeFromSimpleTypeKind(TypeKind type_kind) {
       return JsonType();
     case TYPE_VARIANT:
       return VariantType();
+    case TYPE_OBJECT:
+      return ObjectType();
     default:
       ZETASQL_VLOG(1) << "Could not build static Type from type: "
               << Type::TypeKindToString(type_kind, PRODUCT_INTERNAL);
@@ -1331,6 +1349,8 @@ const ArrayType* ArrayTypeFromSimpleTypeKind(TypeKind type_kind) {
       return JsonArrayType();
     case TYPE_VARIANT:
       return VariantArrayType();
+    case TYPE_OBJECT:
+      return ObjectArrayType();
     default:
       ZETASQL_VLOG(1) << "Could not build static ArrayType from type: "
               << Type::TypeKindToString(type_kind, PRODUCT_INTERNAL);
