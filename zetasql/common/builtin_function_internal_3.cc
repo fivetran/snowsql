44f9396fa48c4a02fd63a2a12bf5780cdb6e959d
Pavel Grivachev <pavel@fivetran.com>
2023-07-28 14:37:32 +0200

    add more aliases

--- zetasql/common/builtin_function_internal_3.cc
+++ zetasql/common/builtin_function_internal_3.cc
@@ -4784,7 +4784,7 @@ void GetSnowflakeDateAndTimeFunctions(TypeFactory* type_factory,
   InsertFunction(
       functions, options, "date_from_parts", SCALAR,
       {{date_type, {double_type, double_type, double_type}, FN_DATE_FROM_PARTS}},
-      fn_options);
+      FunctionOptions().set_alias_name("datefromparts"));
 
   // CONVERT_TIMEZONE
   InsertFunction(
@@ -4800,19 +4800,26 @@ void GetSnowflakeDateAndTimeFunctions(TypeFactory* type_factory,
        {int64_type, {string_type, timestamp_type, timestamp_type}, FN_TIMEDIFF_TIMESTAMP}},
       fn_options);
 
-  // TIMESTAMP_FROM_PARTS, TIMESTAMP_LTZ_FROM_PARTS
+  // TIMESTAMP_FROM_PARTS, 
   InsertFunction(
       functions, options, "timestamp_from_parts", SCALAR,
       {{timestamp_type, {double_type, double_type, double_type, double_type, double_type, double_type, {double_type, OPTIONAL}}, FN_TIMESTAMP_FROM_PART_DOUBLE},
        {timestamp_type, {date_type, time_type}, FN_TIMESTAMP_FROM_PART_DATE}},
-      FunctionOptions().set_alias_name("timestamp_ltz_from_parts"));
+      FunctionOptions().set_alias_name("timestampfromparts"));
 
-  // TIMESTAMP_NTZ_FROM_PARTS, TIMESTAMP_TZ_FROM_PARTS
+  // TIMESTAMP_LTZ_FROM_PARTS, TIMESTAMP_NTZ_FROM_PARTS
   InsertFunction(
-      functions, options, "timestamp_ntz_from_parts", SCALAR,
-      {{timestamp_type, {double_type, double_type, double_type, double_type, double_type, double_type, {double_type, OPTIONAL}, {string_type, OPTIONAL}}, FN_TIMESTAMP_FROM_PART_DOUBLE},
-       {timestamp_type, {date_type, time_type}, FN_TIMESTAMP_FROM_PART_DATE}},
-      FunctionOptions().set_alias_name("timestamp_tz_from_parts"));
+    functions, options, "timestamp_ltz_from_parts", SCALAR,
+    {{timestamp_type, {double_type, double_type, double_type, double_type, double_type, double_type, {double_type, OPTIONAL}}, FN_TIMESTAMP_FROM_PART_DOUBLE},
+     {timestamp_type, {date_type, time_type}, FN_TIMESTAMP_FROM_PART_DATE}},
+    FunctionOptions().set_alias_name("timestamp_ntz_from_parts"));
+
+  // TIMESTAMP_TZ_FROM_PARTS
+  InsertFunction(
+      functions, options, "timestamp_tz_from_parts", SCALAR,
+      {{timestamp_type, {double_type, double_type, double_type, double_type, double_type, double_type, {double_type, OPTIONAL}, {string_type, OPTIONAL}}, FN_TIMESTAMP_TZ_FROM_PART_DOUBLE},
+       {timestamp_type, {date_type, time_type}, FN_TIMESTAMP_TZ_FROM_PART_DATE}},
+      fn_options);
 }
 
 void GetSnowflakeSemiStructuredFunctions(TypeFactory* type_factory,
